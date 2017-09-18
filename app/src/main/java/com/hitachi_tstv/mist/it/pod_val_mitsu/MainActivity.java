c5187f7abbdbe07c14dcab21df4f047085c1baeb
evijung <evides01@gmail.com>
2017-09-18 14:32:17 +0700

    ui 18/09/2017 14.32

--- app/src/main/java/com/hitachi_tstv/mist/it/pod_val_mitsu/MainActivity.java
+++ app/src/main/java/com/hitachi_tstv/mist/it/pod_val_mitsu/MainActivity.java
@@ -1,13 +1,39 @@
 package com.hitachi_tstv.mist.it.pod_val_mitsu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.imgMALogo)
+    ImageView logoImageView;
+    @BindView(R.id.edtMAUsername)
+    EditText usernameEditText;
+    @BindView(R.id.edtMAPassword)
+    EditText passwordEditText;
+    @BindView(R.id.btnMALogin)
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+
+    }
+
+    @OnClick(R.id.btnMALogin)
+    public void onViewClicked() {
+        Intent intent = new Intent(MainActivity.this, TripActivity.class);
     }
 }
---
+++ app/src/main/java/com/hitachi_tstv/mist/it/pod_val_mitsu/MyConstant.java
@@ -0,0 +1,9 @@
+package com.hitachi_tstv.mist.it.pod_val_mitsu;
+
+/**
+ * Created by Tunyaporn on 9/18/2017.
+ */
+
+public interface MyConstant {
+
+}
